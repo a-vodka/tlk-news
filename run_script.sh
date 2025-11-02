fc5b5e540b12adb474414d6ff7d28585619594f1
Oleksii Vodka <oleksii.vodka@gmail.com>
2025-11-02 17:27:19 +0200

    add log messages to file

--- run_script.sh
+++ run_script.sh
@@ -6,7 +6,7 @@ cd /home/oleksii/tlk-news/
 # Infinite loop to restart script on failure
 while true; do
     echo "Starting $SCRIPT..."
-    /home/oleksii/bin/python3 "$SCRIPT"
+    /home/oleksii/bin/python3 "$SCRIPT" >> output.log
     
     echo "Script crashed or exited. Restarting in 5 seconds..."
     sleep 5
